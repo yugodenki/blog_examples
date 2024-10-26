d3fa42043e1a1588955f474d184376fcc3794899
Yugo <134839060+yugodenki@users.noreply.github.com>
2024-10-26 16:40:31 +0900

    Separated images by size, created converter for RGB444

--- image_to_header/converter_rgb444.py
+++ image_to_header/converter_rgb444.py
@@ -5,21 +5,21 @@ import numpy as np
 """
 Converts images to header files
 Each pixel will be read as 24-bit RGB (8 bits each) 
-and will be converted into RGB565
+and will be converted into RGB444
 """
 for i in range(5):
   # Open images
   file_name = f"frame{i:02d}"
-  image_path = f"../data/320x240images/{file_name}.png"
-  img = np.array(Image.open(image_path).convert("RGB"))
+  image_path = f"../data/280x240images/{file_name}.png"
+  img = np.array(Image.open(image_path))
 
   # Create a C header file to store the image as an array
-  with open(f"../data/320x240images/{file_name}_rgb565.h", "w") as f:
+  with open(f"../data/280x240images/{file_name}_rgb444.h", "w") as f:
 
     # Add array declaration 
     f.write("const uint16_t {}[{:d}] = {{\n".format(
       file_name,
-      int(img.shape[0] * img.shape[1])
+      int((img.shape[0] * img.shape[1]))
     ))
 
     for row_j, row in enumerate(img):
@@ -28,14 +28,15 @@ for i in range(5):
         r = int(pixel[0])
         g = int(pixel[1])
         b = int(pixel[2])
+
+        r4bits = r >> 4
+        g4bits = g >> 4
+        b4bits = b >> 4
         
-        r5bits = r >> 3
-        g6bits = g >> 2
-        b5bits = b >> 3
+        # The upper 4 bits will be empty
+        rgb444_pixel = (r4bits << 8) | (g4bits << 4) | b4bits
         
-        rgb565_pixel = (r5bits << 11) | (g6bits << 5) | b5bits
-        
-        row_string += f"0x{rgb565_pixel:04X}, "
+        row_string += f"0x{rgb444_pixel:04X}, "
       # Write row to file
       if row_j == img.shape[0]:
         # For the last row, truncate the last comma
