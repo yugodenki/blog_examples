d3c6198a800b8981585922bd08ce066f57b94438
Yugo <134839060+yugodenki@users.noreply.github.com>
2024-10-26 13:23:56 +0900

    Added video-from-pico-lcd-animation example

---
+++ video-from-pico-lcd-animation/README.md
@@ -0,0 +1,77 @@
+# blog/video-from-pico-lcd-animation README (日本語版)
+Raspberry Pi PicoからLCDに映像を出力するサンプルです。
+連続する画像データを表示することで、GIF画像のような映像を表示します。
+画像データはあらかじめCヘッダファイルに変換したものを使用します。
+ 
+## 動作手順
+### 前提
+- Pico SDKがインストールされている必要があります。
+インストールは公式手順「[Getting started with Raspberry Pi Pico-series](https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf)」にしたがってください。
+
+### ビルド手順
+リポジトリをクローンしたあと、もしまだであれば`video-from-pico-lcd-animation`ディレクトリに移動してください。
+
+ビルドファイルを格納するフォルダを作成します：
+
+```bash
+mkdir build
+```
+
+ビルドフォルダに移動します：
+
+```bash
+cd build
+```
+
+CMakeを実行します：
+
+```bash
+cmake ..
+```
+
+`make`を実行します。このコマンドの実行が成功すると`build`フォルダ内に`.uf2`ファイルができているはずです。
+
+```bash
+make
+```
+
+ビルドしたプログラムをRaspberry Pi Pico上で動作させるには、まずPicoのBOOTSELボタンを押しながらPicoをコンピューターに接続します。コンピューターはPicoを大容量ストレージデバイスとして認識するので、先ほどの`.uf2`ファイルをPicoにコピーします。コピーが終了するとただちにPicoは再起動し、プログラムが動作し始めます。
+
+# blog/sound-from-pico README (English)
+This is an example code I used in my blog post to demonstrate outputting animation to an LCD module using the Raspberry Pi Pico.
+It is displaying a sequence of images similar to a GIF.
+The image data is provided as a C header file that is preprepared.
+
+## Building and running the program
+### Prerequisite
+- Pico SDK needs to be installed.
+Please follow the official instruction [Getting started with Raspberry Pi Pico-series](https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf) for the installment.
+
+### Build instructions
+After cloning the repo, move to the `video-from-pico-lcd-animation` directory if not yet.
+
+Create a directory for the output build files:
+
+```bash
+mkdir build
+```
+
+Move to the `build` folder:
+
+```bash
+cd build
+```
+
+Run CMake:
+
+```bash
+cmake ..
+```
+
+Run make. After running this command, you should see a `.uf2` file built within the `build` folder.
+
+```bash
+make
+```
+
+In order to run this program on the Raspberry Pi Pico, connect your Pico to your computer while pressing the BOOTSEL button. Your computer should detect the Pico as a USB mass storage device, so simply copy over the `.uf2` file to Pico. Pico should immediately reboot and start running the program.
---
+++ video-from-pico-lcd-animation/main.c
@@ -0,0 +1,159 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/**
+ * Modifications made by yugodenki, 2024
+ */
+ 
+
+#include <stdio.h>
+
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/gpio.h"
+
+#include "st7789_lcd.pio.h"
+
+#include "../data/images/frame00_rgb565.h"
+#include "../data/images/frame01_rgb565.h"
+#include "../data/images/frame02_rgb565.h"
+#include "../data/images/frame03_rgb565.h"
+#include "../data/images/frame04_rgb565.h"
+
+// Tested with the parts that have the height of 240 and 320
+#define SCREEN_WIDTH 240
+#define SCREEN_HEIGHT 320
+#define IMAGE_SIZE 256
+#define LOG_IMAGE_SIZE 8
+
+#define PIN_DIN 0
+#define PIN_CLK 1
+#define PIN_CS 2
+#define PIN_DC 3
+#define PIN_RESET 4
+#define PIN_BL 5
+
+#define SERIAL_CLK_DIV 1.f
+
+// Format: cmd length (including cmd byte), post delay in units of 5 ms, then cmd payload
+// Note the delays have been shortened a little
+static const uint8_t st7789_init_seq[] = {
+        1, 20, 0x01,                        // Software reset
+        1, 10, 0x11,                        // Exit sleep mode
+        2, 2, 0x3a, 0x55,                   // Set colour mode to 16 bit
+        2, 0, 0x36, 0x00,                   // Set MADCTL: row then column, refresh is bottom to top ????
+        5, 0, 0x2a, 0x00, 0x00, SCREEN_WIDTH >> 8, SCREEN_WIDTH & 0xff,   // CASET: column addresses
+        5, 0, 0x2b, 0x00, 0x00, SCREEN_HEIGHT >> 8, SCREEN_HEIGHT & 0xff, // RASET: row addresses
+        1, 2, 0x21,                         // Inversion on, then 10 ms delay (supposedly a hack?)
+        1, 2, 0x13,                         // Normal display on, then 10 ms delay
+        1, 2, 0x29,                         // Main screen turn on, then wait 500 ms
+        0                                   // Terminate list
+};
+
+static inline void lcd_set_dc_cs(bool dc, bool cs) {
+    sleep_us(1);
+    gpio_put_masked((1u << PIN_DC) | (1u << PIN_CS), !!dc << PIN_DC | !!cs << PIN_CS);
+    sleep_us(1);
+}
+
+static inline void lcd_write_cmd(PIO pio, uint sm, const uint8_t *cmd, size_t count) {
+    st7789_lcd_wait_idle(pio, sm);
+    lcd_set_dc_cs(0, 0);
+    st7789_lcd_put(pio, sm, *cmd++);
+    if (count >= 2) {
+        st7789_lcd_wait_idle(pio, sm);
+        lcd_set_dc_cs(1, 0);
+        for (size_t i = 0; i < count - 1; ++i)
+            st7789_lcd_put(pio, sm, *cmd++);
+    }
+    st7789_lcd_wait_idle(pio, sm);
+    lcd_set_dc_cs(1, 1);
+}
+
+static inline void lcd_init(PIO pio, uint sm, const uint8_t *init_seq) {
+    const uint8_t *cmd = init_seq;
+    while (*cmd) {
+        lcd_write_cmd(pio, sm, cmd + 2, *cmd);
+        sleep_ms(*(cmd + 1) * 5);
+        cmd += *cmd + 2;
+    }
+}
+
+static inline void st7789_start_pixels(PIO pio, uint sm) {
+    uint8_t cmd = 0x2c; // RAMWR
+    lcd_write_cmd(pio, sm, &cmd, 1);
+    lcd_set_dc_cs(1, 0);
+}
+
+int main() {
+    stdio_init_all();
+
+    PIO pio = pio0;
+    uint sm = 0;
+    uint offset = pio_add_program(pio, &st7789_lcd_program);
+    st7789_lcd_program_init(pio, sm, offset, PIN_DIN, PIN_CLK, SERIAL_CLK_DIV);
+
+    gpio_init(PIN_CS);
+    gpio_init(PIN_DC);
+    gpio_init(PIN_RESET);
+    gpio_init(PIN_BL);
+    gpio_set_dir(PIN_CS, GPIO_OUT);
+    gpio_set_dir(PIN_DC, GPIO_OUT);
+    gpio_set_dir(PIN_RESET, GPIO_OUT);
+    gpio_set_dir(PIN_BL, GPIO_OUT);
+
+    gpio_put(PIN_CS, 1);
+    gpio_put(PIN_RESET, 1);
+    lcd_init(pio, sm, st7789_init_seq);
+    gpio_put(PIN_BL, 1);
+
+    uint16_t *frame;
+    int frame_selector = 0;
+
+    while (1) {
+        switch (frame_selector) {
+            case 0:
+            default: 
+                frame = frame00;
+                break;
+            case 1:
+            case 7:
+                frame = frame01;
+                break;
+            case 2:
+            case 6:
+                frame = frame02;
+                break;
+            case 3:
+            case 5:
+                frame = frame03;
+                break;
+            case 4:
+                frame = frame04;
+                break;
+        }
+        if (frame_selector == 7) {
+            frame_selector = 0;
+        } else {
+            frame_selector++;
+        }
+
+        absolute_time_t lcd_write_start = get_absolute_time();
+
+        st7789_start_pixels(pio, sm);
+        for (int y = 0; y < SCREEN_HEIGHT; ++y) {
+            for (int x = 0; x < SCREEN_WIDTH; ++x) {
+                uint16_t colour = frame[SCREEN_WIDTH * y + x];
+                st7789_lcd_put(pio, sm, colour >> 8);
+                st7789_lcd_put(pio, sm, colour & 0xff);
+            }
+        }
+
+        absolute_time_t lcd_write_end = get_absolute_time();
+        int64_t lcd_write_time = absolute_time_diff_us(lcd_write_start, lcd_write_end);
+        printf("%lld us\n", lcd_write_time);
+    }
+}
